871d5d62738ffb63d1c8e41a2d72578724a56cba
565353780 <565353780@qq.com>
2022-05-05 17:22:29 +0800

    add srv file

--- robot_path_planner/include/robot_path_planner/RobotPathPlannerServer.h
+++ robot_path_planner/include/robot_path_planner/RobotPathPlannerServer.h
@@ -2,13 +2,29 @@
 #define ROBOT_PATH_PLANNER_SERVER_H
 
 #include <robot_path_planner/RobotPathPlanner.h>
+#include <robot_path_planner/PointVecToPoseVec.h>
 
 class RobotPathPlannerServer
 {
 public:
-  RobotPathPlannerServer()
+  RobotPathPlannerServer() :
+    get_pose_list_server_(nh_.advertiseService(
+          "robot_path_planner/get_pose_vec",
+          &RobotPathPlannerServer::getPoseVecDiffCallback,
+          this))
   {}
 
+private:
+  bool getPoseVecDiffCallback(
+      robot_path_planner::PointVecToPoseVec::Request &req,
+      robot_path_planner::PointVecToPoseVec::Response &res);
+
+private:
+  RobotPathPlanner robot_path_planner_;
+
+  ros::NodeHandle nh_;
+
+  ros::ServiceServer get_pose_list_server_;
 };
 
 #endif // ROBOT_PATH_PLANNER_SERVER_H
--- robot_path_planner/src/RobotPathPlannerServer.cpp
+++ robot_path_planner/src/RobotPathPlannerServer.cpp
@@ -1,2 +1,8 @@
 #include <robot_path_planner/RobotPathPlannerServer.h>
 
+bool RobotPathPlannerServer::getPoseVecDiffCallback(
+    robot_path_planner::PointVecToPoseVec::Request &req,
+    robot_path_planner::PointVecToPoseVec::Response &res)
+{
+  return true;
+}
